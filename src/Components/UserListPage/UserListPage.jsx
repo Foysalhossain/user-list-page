0fd3209663dd98b6b5cf5a73bced4840a9bf2bd9
Foysal Hossain <foysalhossain772@gmail.com>
2024-02-10 15:25:47 +0600

    seach bar added

--- src/Components/UserListPage/UserListPage.jsx
+++ src/Components/UserListPage/UserListPage.jsx
@@ -4,6 +4,7 @@ import UserCard from "../UserCard/UserCard";
 
 const UserListPage = () => {
     const [users, setUsers] = useState([]);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         fetch('https://dummyjson.com/users')
@@ -11,12 +12,23 @@ const UserListPage = () => {
             .then(data => setUsers(data.users))
     }, []);
 
+
     return (
         <div className="user-list p-20 mx-auto">
-            <h1>User List</h1>
+            <h1 className="text-center text-5xl font-bold">User List</h1>
+            <div className="text-center">
+                <input
+                    type="text"
+                    placeholder="Search By Name"
+                    className="input input-bordered join-item my-8 w-96"
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+            </div>
             <div className="user-cards grid grid-cols-4 justify-items-center gap-6">
                 {
-                    users.map(user => <UserCard key={user.id} user={user}></UserCard>)
+                    users.filter(item => {
+                        return search.toLowerCase() === '' ? item : item.firstName.toLowerCase().includes(search);
+                    }).map(user => <UserCard key={user.id} user={user}></UserCard>)
                 }
             </div>
         </div>
